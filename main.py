27a225b5abb527d4ee68f909452957674b8287ea
Wisdom <mrwhizzy1@gmail.com>
2023-05-09 22:28:27 +0100

    added the date to the png file

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageFont, ImageDraw
+from datetime import date
 
-#for adding the heading text to the image.
 
 image = Image.open("post.png")
 draw = ImageDraw.Draw(image)
+
+#for adding the heading text to the image.
 font = ImageFont.truetype("Inter-Medium.ttf", size=50)
 content = "Latest Weather Forecast!"
 color = "rgb(255, 255, 255)" 
@@ -11,6 +13,14 @@ color = "rgb(255, 255, 255)"
 draw.text((x, y), content, color, font=font)
 
 
-image.save("test.png")
+#for adding the date of the forecast
+font = ImageFont.truetype("Inter-Medium.ttf", size=30)
+today = date.today()
+content = date.today().strftime("%A - %B %m %Y")
+color = "rgb(255, 255, 255)" 
+(x, y) = (45, 145)
+draw.text((x, y), content, color, font=font)
 
+image.show()
+image.save("test.png")
 print("it is working")
