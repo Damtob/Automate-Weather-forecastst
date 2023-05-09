664cbce965f322351047a0aea3799cf704e1879a
Wisdom <mrwhizzy1@gmail.com>
2023-05-09 22:01:52 +0100

    addedd the first heading to the image

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
+
+#for adding the heading text to the image.
+
+image = Image.open("post.png")
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("Inter-Medium.ttf", size=50)
+content = "Latest Weather Forecast!"
+color = "rgb(255, 255, 255)" 
+(x, y) = (45, 55)
+draw.text((x, y), content, color, font=font)
+
+
+image.save("test.png")
+
+print("it is working")
